05e6a33053b8e1520407252c0afce075231bfe75
Darryl <williams.de39@gmail.com>
2020-08-10 17:56:04 -0700

    updated heroku

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,37 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: "burgers_db",
-});
+// var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: process.env.MYSQL_PORT,
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: 'burgers_db',
+    })
+};
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// var connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   port: process.env.MYSQL_PORT,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: "burgers_db",
+// });
+
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+connection.connect();
 
 module.exports = connection;
